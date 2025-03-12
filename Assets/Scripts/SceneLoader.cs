355ddf1962a01c660df0d9516ec8b46b11ae3335
PoyoMain <CLC9512@GMAIL.COM>
2025-03-12 19:58:01 -0400

    Build

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, FullScreenMode.ExclusiveFullScreen);
+    }
     public void GoToScene(int sceneNum)
     {
         SceneManager.LoadScene(sceneNum);
